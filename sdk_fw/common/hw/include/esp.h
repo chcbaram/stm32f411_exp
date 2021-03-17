39d3a4f4e0fce6f3e198357e178836767a2c6d02
Baram <chcbaram@gmail.com>
2021-03-17 21:28:26 +0900

    ESP8266 서버 연결 추가.

--- sdk_fw/common/hw/include/esp.h
+++ sdk_fw/common/hw/include/esp.h
@@ -15,6 +15,22 @@
 
 
 bool espInit(void);
+bool espOpen(uint8_t ch, uint32_t baud);
+bool espIsOpen(void);
+
+uint32_t espAvailable(void);
+uint32_t espWrite(uint8_t *p_data, uint32_t length);
+uint8_t  espRead(void);
+uint32_t espPrintf(char *fmt, ...);
+
+void espLogEnable(void);
+void espLogDisable(void);
+bool espCmd(const char *cmd_str, uint32_t timeout);
+bool espPing(uint32_t timeout);
+bool espWaitOK(uint32_t timeout);
+bool espConnectWifi(char *ssd_str, char *pswd_str, uint32_t timeout);
+bool espClientBegin(char *ip_str, char *port_str, uint32_t timeout);
+bool espClientEnd(void);
 
 #endif
 
