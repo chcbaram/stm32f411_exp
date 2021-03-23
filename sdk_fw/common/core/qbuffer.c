cea42bb5ae43dcb69ac7160db25602a0b486801f
Baram <chcbaram@gmail.com>
2021-03-23 21:21:21 +0900

    qbuffer 데이터 사이즈 처리 추가.

--- sdk_fw/common/core/qbuffer.c
+++ sdk_fw/common/core/qbuffer.c
@@ -24,6 +24,20 @@ bool qbufferCreate(qbuffer_t *p_node, uint8_t *p_buf, uint32_t length)
   p_node->in    = 0;
   p_node->out   = 0;
   p_node->len   = length;
+  p_node->size  = 1;
+  p_node->p_buf = p_buf;
+
+  return ret;
+}
+
+bool qbufferCreateBySize(qbuffer_t *p_node, uint8_t *p_buf, uint32_t size, uint32_t length)
+{
+  bool ret = true;
+
+  p_node->in    = 0;
+  p_node->out   = 0;
+  p_node->len   = length;
+  p_node->size  = size;
   p_node->p_buf = p_buf;
 
   return ret;
@@ -41,9 +55,15 @@ bool qbufferWrite(qbuffer_t *p_node, uint8_t *p_data, uint32_t length)
 
     if (next_in != p_node->out)
     {
-      if (p_node->p_buf != NULL)
+      if (p_node->p_buf != NULL && p_data != NULL)
       {
-        p_node->p_buf[p_node->in] = p_data[i];
+        uint8_t *p_buf;
+
+        p_buf = &p_node->p_buf[p_node->in*p_node->size];
+        for (int i=0; i<p_node->size; i++)
+        {
+          p_buf[i] = p_data[i];
+        }
       }
       p_node->in = next_in;
     }
@@ -64,9 +84,15 @@ bool qbufferRead(qbuffer_t *p_node, uint8_t *p_data, uint32_t length)
 
   for (int i=0; i<length; i++)
   {
-    if (p_node->p_buf != NULL)
+    if (p_node->p_buf != NULL && p_data != NULL)
     {
-      p_data[i] = p_node->p_buf[p_node->out];
+      uint8_t *p_buf;
+
+      p_buf = &p_node->p_buf[p_node->out*p_node->size];
+      for (int i=0; i<p_node->size; i++)
+      {
+        p_data[i] = p_buf[i];
+      }
     }
 
     if (p_node->out != p_node->in)
@@ -82,6 +108,17 @@ bool qbufferRead(qbuffer_t *p_node, uint8_t *p_data, uint32_t length)
 
   return ret;
 }
+
+uint8_t *qbufferPeekWrite(qbuffer_t *p_node)
+{
+  return &p_node->p_buf[p_node->in*p_node->size];
+}
+
+uint8_t *qbufferPeekRead(qbuffer_t *p_node)
+{
+  return &p_node->p_buf[p_node->out*p_node->size];
+}
+
 
 uint32_t qbufferAvailable(qbuffer_t *p_node)
 {
--- sdk_fw/common/core/qbuffer.h
+++ sdk_fw/common/core/qbuffer.h
@@ -23,6 +23,7 @@ typedef struct
   uint32_t in;
   uint32_t out;
   uint32_t len;
+  uint32_t size;
 
   uint8_t *p_buf;
 } qbuffer_t;
@@ -30,8 +31,11 @@ typedef struct
 
 void     qbufferInit(void);
 bool     qbufferCreate(qbuffer_t *p_node, uint8_t *p_buf, uint32_t length);
+bool     qbufferCreateBySize(qbuffer_t *p_node, uint8_t *p_buf, uint32_t size, uint32_t length);
 bool     qbufferWrite(qbuffer_t *p_node, uint8_t *p_data, uint32_t length);
 bool     qbufferRead(qbuffer_t *p_node, uint8_t *p_data, uint32_t length);
+uint8_t *qbufferPeekWrite(qbuffer_t *p_node);
+uint8_t *qbufferPeekRead(qbuffer_t *p_node);
 uint32_t qbufferAvailable(qbuffer_t *p_node);
 void     qbufferFlush(qbuffer_t *p_node);
 
